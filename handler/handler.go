e287524d5e6eff61af08aaf916440eb2979148d4
Richard Nestor Lartiga Mena <halrrik@gmail.com>
2023-10-04 14:32:25 -0300

    adicionar archivos diferentes para handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,37 +1,31 @@
 package handler
 
-import (
-	"net/http"
+// func CreateOpeningHandler(ctx *gin.Context) {
+// 	ctx.JSON(http.StatusOK, gin.H{
+// 		"msg": "POST Opening texto texto",
+// 	})
+// }
 
-	"github.com/gin-gonic/gin"
-)
+// func ShowOpeningHandler(ctx *gin.Context) {
+// 	ctx.JSON(http.StatusOK, gin.H{
+// 		"msg": "GET Opening texto texto",
+// 	})
+// }
 
-func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening texto texto",
-	})
-}
+// func DeleteOpeningHandler(ctx *gin.Context) {
+// 	ctx.JSON(http.StatusOK, gin.H{
+// 		"msg": "delete Opening texto texto",
+// 	})
+// }
 
-func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening texto texto",
-	})
-}
+// func UpdateOpeningHandler(ctx *gin.Context) {
+// 	ctx.JSON(http.StatusOK, gin.H{
+// 		"msg": "put Opening texto texto",
+// 	})
+// }
 
-func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "delete Opening texto texto",
-	})
-}
-
-func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "put Opening texto texto",
-	})
-}
-
-func ListOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening texto texto",
-	})
-}
+// func ListOpeningHandler(ctx *gin.Context) {
+// 	ctx.JSON(http.StatusOK, gin.H{
+// 		"msg": "GET Opening texto texto",
+// 	})
+// }
